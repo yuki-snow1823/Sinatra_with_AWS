ce040dfcb66b082172add31fb49fe6d3533efe18
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-09-05 23:56:35 +0900

    setting puma

--- ruby_api_server/myapp.rb
+++ ruby_api_server/myapp.rb
@@ -1,4 +1,13 @@
 require 'sinatra'
-get '/frank-says' do
-  'Put this in your pipe & smoke it!'
+
+configure {
+  set :server, :puma
+}
+
+class Pumatra < Sinatra::Base
+  get '/hello' do
+    return 'hello pumatra!'
+  end
+
+  run! if app_file == $0
 end
