d812a8b69f9591af3259eeeddc776574da35fe51
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-09-05 23:31:25 +0900

    add: todo_app

---
+++ front_todo_app/main.js
@@ -0,0 +1,22 @@
+'use strict';
+const addTaskTrigger = document.getElementsByClassName('js-addTask-trigger')[0];
+const addTaskTarget = document.getElementsByClassName('js-addTask-target')[0];
+const addTaskValue = document.getElementsByClassName('js-addTask-value')[0];
+const removeTask = removeButton => {
+  const targetTask = removeButton.closest('li');
+  addTaskTarget.removeChild(targetTask);
+};
+const addTask = task => {
+  const listItem = document.createElement('li');
+  const removeButton = document.createElement('button');
+  removeButton.innerText = '削除';
+  removeButton.addEventListener('click', () => removeTask(removeButton));
+  listItem.innerText = task;
+  listItem.append(removeButton);
+  addTaskTarget.appendChild(listItem);
+};
+addTaskTrigger.addEventListener('click', event => {
+  const task = addTaskValue.value;
+  addTask(task);
+  addTaskValue.value = '';
+});
