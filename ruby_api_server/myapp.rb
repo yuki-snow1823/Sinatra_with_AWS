9c5be377cfa3c97c0a7d5dfafddbc5e3baac2a9e
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2021-09-08 22:48:28 +0900

    config cross originz

--- ruby_api_server/myapp.rb
+++ ruby_api_server/myapp.rb
@@ -1,13 +1,21 @@
 require 'sinatra'
+require 'sinatra/cross_origin'
 
 configure {
   set :server, :puma
 }
 
+configure do
+  enable :cross_origin
+end
+
 class Pumatra < Sinatra::Base
-  get '/hello' do
+
+  get '/' do
     return 'hello pumatra!'
   end
-
+  # 固定のjson返す
+  # awsへアップ
+  # ローカルで叩く
   run! if app_file == $0
 end
